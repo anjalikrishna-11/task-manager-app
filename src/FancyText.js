9e2b6359fda2bf5beefa7b580c9af0d90043a5ba
anjalikrishna-11 <123520060+anjalikrishna-11@users.noreply.github.com>
2024-07-12 17:06:22 +0530

    Create fancytext.js

---
+++ src/FancyText.js
@@ -0,0 +1,13 @@
+// src/FancyText.js
+import React from 'react';
+import './FancyText.css';
+
+const FancyText = ({ title, text }) => {
+  return (
+    <div className={title ? 'title' : 'regular'}>
+      {text}
+    </div>
+  );
+};
+
+export default FancyText;
