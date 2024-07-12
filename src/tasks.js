7f3d5efd2f650f4ebc35074fa66ac329dd38e0ed
anjalikrishna-11 <123520060+anjalikrishna-11@users.noreply.github.com>
2024-07-12 17:05:41 +0530

    Create tasks.js

---
+++ src/tasks.js
@@ -0,0 +1,12 @@
+// src/tasks.js
+const tasks = [
+    { id: 1, name: "Complete the React challenge", isCompleted: false },
+    { id: 2, name: "Read about React hooks", isCompleted: false },
+    { id: 3, name: "Build a Todo App", isCompleted: false },
+    { id: 4, name: "Update tasks on Notion", isCompleted: false },
+    { id: 5, name: "Reach out to advisor", isCompleted: false },
+    
+  ];
+  
+  export default tasks;
+  
